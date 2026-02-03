6e20384194b25d25f464b6462f3b18683dce014e
payal sikarwar <payal25csu346@ncuindia.edu>
2026-02-04 00:40:46 +0530

    Create q3.c

---
+++ q3.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int f;
+    cout << "enter value of farehnite"<< endl;
+    cin >> f;
+    cout << "temperature in celcius" << endl;
+    float c = (f-32)*0.556;
+    cout << c << endl;
+
+}
